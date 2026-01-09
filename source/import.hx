08bdca621e2dbf79e932e447e7b3ac8dd5207563
CybrrPortGames <dalucanogheaven12@gmail.com>
2026-01-09 17:29:58 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -58,6 +58,22 @@ import sys.thread.Thread;
 import crowplexus.iris.Iris;
 #end
 
+import mobile.backend.SUtil;
+//import mobile.backend.MobileScaleMode;
+   
+ //Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 //Flixel
 import flixel.sound.FlxSound;
 import flixel.FlxG;
