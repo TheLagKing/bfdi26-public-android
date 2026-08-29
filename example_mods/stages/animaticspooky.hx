1f846c80da6c4009d1112379ea547dfa6c5fb491
TheLagKing <dalucanogheaven12@gmail.com>
2026-08-29 13:54:56 +0800

    Update animaticspooky.hx

--- example_mods/stages/animaticspooky.hx
+++ example_mods/stages/animaticspooky.hx
@@ -118,7 +118,7 @@ function onBeatHit()
 				bg.loadGraphic(Paths.image('backgrounds/funnyfellow/spooky/evilbg'));
 				flashing = false;
 				killStuff.kill();
-				screen.alpha = 1;
+				screen.alpha = 0.65;
 				shader.setFloat("amount", 0.02);
 			case 332: camGame.visible = false;
 		}
