5a269d0b3ec30984cc816a436877025e71a268af
TheLagKing <dalucanogheaven12@gmail.com>
2026-09-05 09:21:41 +0800

    Change blend mode from 'lighten' to 'screen'

--- example_mods/stages/airy.lua
+++ example_mods/stages/airy.lua
@@ -113,7 +113,7 @@ addLuaSprite('v',true)
 makeLuaSprite('r', 'backgrounds/oneshot/overlay',100,0)
 setProperty('r.visible',false)
 setObjectCamera('r','camHUD')
-setBlendMode('r','lighten')
+setBlendMode('r','screen')
 addLuaSprite('r',false)
 --
 
